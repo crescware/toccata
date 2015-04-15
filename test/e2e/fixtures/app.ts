3692a607e8d44df7be7f5d595d9ec13e8acb82af
armorik83 <okunokentaro@me.com>
2015-04-16 01:18:54 +0900

    test(fixtures): update

--- test/e2e/fixtures/app.ts
+++ test/e2e/fixtures/app.ts
@@ -2,4 +2,7 @@
 
 import angular from './take-angular';
 import toccata from '../../../src/toccata';
-console.log(toccata(angular));
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('toccata-mode').innerText = toccata(angular).operatingMode;
+});
