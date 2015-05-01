c478ee8d5728bce12b5f2fb18c66036bb49c99da
armorik83 <okunokentaro@me.com>
2015-05-01 14:01:52 +0900

    feat(index): create index

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _interopRequireDefault = function (obj) { return obj && obj.__esModule ? obj : { 'default': obj }; };
+
+var _default2 = require('./lib/toccata');
+
+var _default3 = _interopRequireDefault(_default2);
+
+exports['default'] = _default3['default'];
+module.exports = exports['default'];
+
+// same as the following ES6
+// export default from './lib/toccata';
