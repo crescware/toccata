832f855923316816a209b36c20049e4372a2e855
armorik83 <okunokentaro@me.com>
2015-05-02 17:52:08 +0900

    chore(take-angular): flatten

--- test/e2e/fixtures/take-angular.ts
+++ test/e2e/fixtures/take-angular.ts
@@ -1,11 +1,10 @@
 /// <reference path="../../../typings/node/node.d.ts" />
 'use strict';
+var angular1 = require('angular');
+var angular2 = require('angular2/angular2');
 
 const angular = (() => {
-  const pkg: any = require('angular');
-  return (pkg === 'dummy')
-    ? require('angular2/angular2')
-    : pkg;
+  return (angular1 === 'dummy') ? angular2 : angular1;
 })();
 
 export default angular;
